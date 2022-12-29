0b872dd51d95e9b9e7560a728dcf38b60a9b9fbf
Denis <dfazylov@mail.ru>
2022-12-29 22:34:35 +0500

    save

---
+++ Лекции/Лекция_1/hello.py
@@ -0,0 +1,64 @@
+#print ('hello world')
+#value = None
+#print(type(value))
+#a = 123
+#b = 1.23
+#print(type(a))
+#print(type(b))
+#value = 12334
+#print(type(value))
+#s = 'hello "world"'
+#print(s) #вывод строки
+
+#print(a, '-', b, '-', s)
+#print('{1} - {2} - {0}'.format(a, b, s))
+#print(f'{a} - {b} - {s}')
+
+#f = True
+#print(f)
+
+#list = ['1', '2', '3']
+#col = ['hello', 1,2,3,4.5, True]
+#print(list)
+#print(col)
+
+
+
+# print('Введите a')
+# a = float(input())
+# print('Введите b')
+# b = float(input())
+# print(a, ' ', b, ' ', a+b)
+# # print('{} {}'.format(a,b))
+# # print(f'{a} {b}')
+
+
+# func = 1
+# T = 4
+# x = 2
+# print(func<T>(x))
+
+
+
+# a = int(input())
+# b = int(input())
+# if a > b:
+#     print(a)
+# else:
+#     print(b)  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
